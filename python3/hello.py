b4bfe79d256c952c9f5f9c30b17ac2da839de8e6
esther <ribeiroesther54@gmail.com>
2024-08-20 12:41:12 -0300

    atualização

--- python3/hello.py
+++ python3/hello.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
+""" Programa Multi Línguas
+
+Esse código retorna o "Hello World" baseado na língua que está instalada no 
+sistema operacional.
+
+Como usar o programa: 
+Tenha a variável LANG corretamente configurada, exemplo:
+
+        export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+        ou
+    ./hello.py (para executar usando esse comando, será necessário configurar
+    a permissão do usário chmod +x hello.py)
+"""
+__version__ ="0.0.1"
+__autho__ = "Esther Ribeiro"
+__license__ = "Unlicense"
+
 
 print("Hello world!!!")
+
