10361909c68ff2d9e8ffbd8e514524c595fa5543
esther <ribeiroesther54@gmail.com>
2024-08-21 23:44:04 -0300

    programa multi linguas

--- python3/hello.py
+++ python3/hello.py
@@ -20,6 +20,23 @@ __version__ ="0.0.1"
 __autho__ = "Esther Ribeiro"
 __license__ = "Unlicense"
 
+import os
+# a função GETEVEN pede a variável de sistema LANG que possibilita o módulo  
+#'OS' ter acesso diretamente a configuranção de idioma do ambiente(sistema operacional).
+#foi necessário o acréscimo do [:5] para pegar somente os cinco primeiros 
+#digítos da configuração do idioma, que pode padrão vem escrito dessa forma 
+#nos sistema operacionais : 'pt_BR.utf8'
+# Caso o sistema opereacional não tenha nenhuma configuração do GETEVEN por 
+#padrão o msg vai ser traduzido para o inglês.
+current_language = os.getenv("LANG", "en_US")[:5]
 
-print("Hello world!!!")
+msg ="Hello World!!!" 
+
+if current_language == "pt_BR":
+    msg = "Olá Mundo!!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+
+print(msg)
 
