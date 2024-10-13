1b1f52bed45d64b657acdb4046d2be6bec572f14
esther <ribeiroesther54@gmail.com>
2024-10-12 22:11:08 -0300

    tabuada

--- python3/tabuada.py
+++ python3/tabuada.py
@@ -4,13 +4,13 @@ __version__ = "0.1.0"
 __author__ = "Esther Ribeiro"
 
 numeros =list( range(1,11))
-#mult = numero * outro_numero
+
 
 for numero in numeros:
     print("Tabuada do:", numero)
     for outro_numero in numeros:
         print(numero * outro_numero)
-        #print(numero "x" outro_numero "=" mult)
+       
     print("*********************")
     
 
